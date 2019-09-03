b02b09819f8a4dc805c154ce1ea006a0829d7bf9
Titus Buchanan Jr <titusbuchananjr@gmail.com>
2019-09-03 19:35:26 -0400

    themetoggle

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-//lesson 
+//lesson 6
 
 
 import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <ThemeContextProvider>
         <Navbar />
         <BookList />
+        <ThemeToggle />
       </ThemeContextProvider>
     </div>
   );
