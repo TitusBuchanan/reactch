505df89775dce7572237dac7d74d7fb732d72ee1
Titus Buchanan Jr <titusbuchananjr@gmail.com>
2019-09-03 19:27:06 -0400

    ThemeContextP

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-//lesson 1
+//lesson 
+
 
 import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
